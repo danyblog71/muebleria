06f328f71925eeeac79158a5b7653caec492438b
Daniel <danyblog71@gmail.com>
2020-05-01 23:54:32 -0500

    migrations

---
+++ database/migrations/2020_05_02_033422_create_product_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('pieces');
+            $table->string('measure')->nullable();
+            $table->double('weight',2)->nullable();
+            $table->string('material')->nullable();
+            $table->string('color')->nullable();
+            $table->string('description')->nullable();
+            $table->bigInteger('id_product')->unsigned();
+            $table->timestamps();
+            $table->foreign('id_product')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_details');
+    }
+}
